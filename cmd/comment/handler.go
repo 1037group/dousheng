48304c2c0bc5c214570f55b4f26cb6e50849feec
1638401615 <74094126+1638401615@users.noreply.github.com>
2023-02-06 18:34:55 +0800

    complete basic code

--- cmd/comment/handler.go
+++ cmd/comment/handler.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"github.com/1037group/dousheng/dal/db"
 	douyin_comment "github.com/1037group/dousheng/kitex_gen/douyin_comment"
+	"github.com/1037group/dousheng/kitex_gen/douyin_user"
+	"github.com/1037group/dousheng/pack"
+	"github.com/1037group/dousheng/pkg/configs/sql"
+	"github.com/1037group/dousheng/pkg/errno"
 	"github.com/cloudwego/kitex/pkg/klog"
+	"time"
 )
 
 // CommentServiceImpl implements the last service interface defined in the IDL.
@@ -11,14 +17,97 @@ type CommentServiceImpl struct{}
 
 // CommentAction implements the CommentServiceImpl interface.
 func (s *CommentServiceImpl) CommentAction(ctx context.Context, req *douyin_comment.CommentActionRequest) (resp *douyin_comment.CommentActionResponse, err error) {
-	// TODO: Your code here...
+
 	klog.CtxInfof(ctx, "[CommentAction] %+v", req)
 
-	return
+	switch req.ActionType {
+	case 1:
+		t := time.Now()
+		comment := sql.Comment{
+			CommentId:      *req.CommentId,
+			VideoId:        req.VideoId,
+			UserId:         req.UserId,
+			CommentContent: *req.CommentText,
+			Ctime:          t,
+			Utime:          t,
+		}
+		err = db.CreateComment(ctx, db.DB, &comment)
+		if err != nil {
+			klog.CtxInfof(ctx, err.Error())
+			return nil, errno.ParamErr
+		}
+
+		user, err := db.GetUserByID(ctx, db.DB, req.UserId)
+		if err != nil {
+			klog.CtxInfof(ctx, err.Error())
+			return nil, err
+		}
+
+		msg := "post comment success"
+		userinfo := douyin_user.User{
+			Id:            user.UserId,
+			Name:          user.UserName,
+			FollowCount:   &user.UserFollowCount,
+			FollowerCount: &user.UserFollowerCount,
+			IsFollow:      true, //自己post的评论，默认true
+		}
+		commentinfo := pack.Comment(&comment, &userinfo)
+		return &douyin_comment.CommentActionResponse{
+			StatusCode: 0,
+			StatusMsg:  &msg,
+			Comment:    commentinfo,
+		}, nil
+	case 2:
+		db.DeleteComment(ctx, db.DB, *req.CommentId)
+		msg := "delete comment success"
+		return &douyin_comment.CommentActionResponse{
+			StatusCode: 0,
+			StatusMsg:  &msg,
+			Comment:    nil,
+		}, nil
+	default:
+		klog.CtxInfof(ctx, "action type error\n")
+		return nil, nil
+	}
+
 }
 
 // CommentList implements the CommentServiceImpl interface.
 func (s *CommentServiceImpl) CommentList(ctx context.Context, req *douyin_comment.CommentListRequest) (resp *douyin_comment.CommentListResponse, err error) {
-	// TODO: Your code here...
-	return
+
+	klog.CtxInfof(ctx, "[CommentList]: %v\n", req)
+
+	res, err := db.MGetCommentByVideoId(ctx, db.DB, &req.VideoId)
+	if err != nil {
+		klog.CtxInfof(ctx, err.Error())
+		return nil, err
+	}
+	klog.CtxInfof(ctx, "res: %v\n", res)
+	userMap := make(map[int64]douyin_user.User)
+	var userIDs []int64
+	for _, m := range res {
+		userIDs = append(userIDs, m.UserId)
+	}
+	users, err := db.MGetUserByID(ctx, db.DB, userIDs)
+	if err != nil {
+		klog.CtxInfof(ctx, err.Error())
+		return nil, err
+	}
+	for _, m := range users {
+
+		userMap[m.UserId] = douyin_user.User{
+			Id:            m.UserId,
+			Name:          m.UserName,
+			FollowCount:   &m.UserFollowCount,
+			FollowerCount: &m.UserFollowerCount,
+			IsFollow:      false,
+		}
+	}
+
+	commentList := pack.Comments(res, userMap)
+	return &douyin_comment.CommentListResponse{
+		StatusCode:  0,
+		StatusMsg:   "string",
+		CommentList: commentList,
+	}, nil
 }
--- pack/api.go
+++ pack/api.go
@@ -2,6 +2,7 @@ package pack
 
 import (
 	"github.com/1037group/dousheng/cmd/api/biz/model/douyin_api"
+	"github.com/1037group/dousheng/kitex_gen/douyin_comment"
 	"github.com/1037group/dousheng/kitex_gen/douyin_feed"
 	"github.com/1037group/dousheng/kitex_gen/douyin_publish"
 )
@@ -82,5 +83,68 @@ func PublishListResponseRpc2Api(m *douyin_publish.PublishListResponse) *douyin_a
 		StatusCode: m.StatusCode,
 		StatusMsg:  m.StatusMsg,
 		VideoList:  videoList,
+	}
+}
+
+func CommentListResponseRpc2Api(m *douyin_comment.CommentListResponse) *douyin_api.CommentListResponse {
+	if m == nil {
+		return nil
+	}
+
+	var commentList []*douyin_api.Comment
+
+	for _, comment := range m.CommentList {
+		var user douyin_api.User
+		if comment.User != nil {
+			user = douyin_api.User{
+				ID:            comment.User.Id,
+				Name:          comment.User.Name,
+				FollowCount:   comment.User.FollowCount,
+				FollowerCount: comment.User.FollowerCount,
+				IsFollow:      comment.User.IsFollow,
+			}
+		}
+		one := douyin_api.Comment{
+			ID:         comment.Id,
+			User:       &user,
+			Content:    comment.Content,
+			CreateDate: comment.CreateDate,
+		}
+		commentList = append(commentList, &one)
+	}
+
+	return &douyin_api.CommentListResponse{
+		StatusCode:  m.StatusCode,
+		StatusMsg:   m.StatusMsg,
+		CommentList: commentList,
+	}
+}
+
+func CommentActionResponseRpc2Api(m *douyin_comment.CommentActionResponse) *douyin_api.CommentActionResponse {
+	if m == nil {
+		return nil
+	}
+
+	var user douyin_api.User
+	if m.Comment.User != nil {
+		user = douyin_api.User{
+			ID:            m.Comment.User.Id,
+			Name:          m.Comment.User.Name,
+			FollowCount:   m.Comment.User.FollowCount,
+			FollowerCount: m.Comment.User.FollowerCount,
+			IsFollow:      m.Comment.User.IsFollow,
+		}
+	}
+	comment := douyin_api.Comment{
+		ID:         m.Comment.Id,
+		User:       &user,
+		Content:    m.Comment.Content,
+		CreateDate: m.Comment.CreateDate,
+	}
+
+	return &douyin_api.CommentActionResponse{
+		StatusCode: m.StatusCode,
+		StatusMsg:  m.StatusMsg,
+		Comment:    &comment,
 	}
 }
