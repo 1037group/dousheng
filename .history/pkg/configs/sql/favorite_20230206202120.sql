1cf1c1aa63aa1a33a3febdf9ce9ca9c8eb4fa684
SOULOFCINDER <841135647@qq.com>
2023-02-10 14:36:14 +0800

    delete history

--- .history/pkg/configs/sql/favorite_20230206202120.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `favorite` (
-                            -- `favorite_id` bigint NOT NULL AUTO_INCREMENT,
-                            `user_id` bigint NOT NULL,
-                           `video_id` bigint NOT NULL,
-                           `isfavorite` tinyint NOT NULL,
-                           `delstate` tinyint NOT NULL,
-                           `utime` datetime NOT NULL,
-                           UNIQUE KEY `uk_user_video_id` (`user_id`,`video_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
